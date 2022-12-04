15e3ced0f5eab9931a1ca5f157cd1ef227a13ac6
Kevin <sibusiso.ntuli10@gmail.com>
2022-12-04 13:01:15 -0800

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    user = argv[1:]
+    siz = len(userin)
+    print("{:d} {:s}{:s}".
+         format(siz,
+                "arguments" if (siz) is not 1 else "argument",
+                "." if (siz) is 0 else ":"))
+    for idx, arg in enumerate(user):
+        print("{:d}: {:s}".format(idx + 1, arg))
