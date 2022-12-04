c5ba56294a2e43159028bbbd087278cfe7250a83
Kevin <sibusiso.ntuli10@gmail.com>
2022-12-04 12:29:24 -0800

    Task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+
+    c = 1
+    d = 2
+    print("{} + {} = {}".format(c, d, add(c, d)))
+
