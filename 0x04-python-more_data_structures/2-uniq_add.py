c66d5dc2178f5537cd7e5fbbece4b9246550e6b2
Kevin <sibusiso.ntuli10@gmail.com>
2022-12-10 10:47:00 -0800

    Task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uniq_add(m_list=[]):
+    """Add all unique integers in a list (once for each integer)."""
+    answer = 0
+    for x in set(m_list):
+        answer += x
+        return (answer)
