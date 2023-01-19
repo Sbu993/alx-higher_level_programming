fe91151ab021e74282cf92d4c425b1d2975e9308
Sbu993 <sibusiso.ntuli10@gmail.com>
2023-01-19 12:01:33 -0800

    Task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -11,10 +11,16 @@ class Student():
     Public Attributes: first_name last_name age
     """
     def __init__(self, first_name, last_name, age):
+        """
+        Initializes student with full name and age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """
+        Returns dictionary description with simple data structure
+        """
         return self.__dict__
 
